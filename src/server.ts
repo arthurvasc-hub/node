7b786439f8081201e835a9dd41cdbdf8acaa93e7
arthurvasc-hub <arthurvasc2@gmail.com>
2024-04-27 17:09:22 -0300

    estudos

--- src/server.ts
+++ src/server.ts
@@ -10,10 +10,9 @@ server.set('view engine', 'mustache');
 server.set('views',path.join(__dirname, 'views'));
 server.engine('mustache', mustache());
 
-
 server.use(express.static(path.join(__dirname, '../public')));
 
-
+server.use(express.urlencoded({extended:true}))
 
 server.use(mainRoutes);
 server.use('/CT', CtRouts);
